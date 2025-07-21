b8039bad0514b206c055e3a7ecae6dcdd38ebba8
eric <stupidchen@gmail.com>
2025-07-21 22:04:31 +0800

    eee

--- app/controller/User.php
+++ app/controller/User.php
@@ -20,4 +20,53 @@ class User extends Base {
         return $this->remoteRequest($targetUrl, $this->jsonBody);
     }
 
+    /*
+     * 保存用户昵称
+     */
+    public function saveNickname() {
+        $openid = $this->openid;
+
+        $data = $this->jsonBody;
+        $nickName = $data['nick_name'];
+        // 写入数据库
+        $user = User::where('openid', $openid)->find();
+        if ($user) {
+            $user->nick_name = $nickName;
+            $user->save();
+        } else {
+            User::create([
+                'openid' => $openid,
+                'nick_name' => $nickName
+            ]);
+        }
+
+        $rst['code'] = 200;
+        return json($rst);
+    }
+
+    /*
+     * 保存用户头像
+     */
+    public function saveAvatarUrl() {
+        $openid = $this->openid;
+        $data = $this->jsonBody;
+
+        $avatarUrl = $data['avatar_url'];
+
+
+        // 写入数据库
+        $user = User::where('openid', $openid)->find();
+        if ($user) {
+            $user->avatar_url = $avatarUrl;
+            $user->save();
+        } else {
+            User::create([
+                'openid' => $openid,
+                'avatar_url' => $avatarUrl,
+            ]);
+        }
+
+        $rst['code'] = 200;
+        return json($rst);
+    }
 }
