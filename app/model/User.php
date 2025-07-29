17fa1a0fcefe8fd7cd40f3c76c91e2813e1d322a
eric <stupidchen@gmail.com>
2025-07-29 23:53:30 +0800

    new3

---
+++ app/model/User.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\model;
+
+use think\Model;
+
+class User extends Model
+{
+    // 对应表名
+    protected $name = 'user';
+
+    // 主键字段
+    protected $pk = 'id';
+
+    // 不启用自动时间戳（你使用的是 int 类型）
+    public $autoWriteTimestamp = false;
+
+    // 字段类型转换
+    protected $type = [
+        'id'            => 'integer',
+        'openid'        => 'string',
+        'nick_name'       => 'string',
+        'avata_url'       => 'string',
+        'create_time'   => 'integer',
+        'update_time'   => 'integer',
+        'del_flg'       => 'integer',
+    ];
+}
