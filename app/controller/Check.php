c6ed625a3cd28203812bf9fbc39f164d86f5eef4
eric <stupidchen@gmail.com>
2025-08-01 23:21:32 +0800

    wow2

--- app/controller/Check.php
+++ app/controller/Check.php
@@ -2,7 +2,18 @@
 
 namespace app\controller;
 
+use think\facade\Db;
+
 class Check extends Base {
+
+    public function baseInfo(){
+        $openid = $this->openid;
+        $user = Db::name('user')->field('nick_name,avatar_url')->where('openid', $openid)->find();
+        $rst['code'] = 200;
+        $rst['data'] = $user;
+        return json($rst);
+    }
+
     public function userInfo() {
         $targetUrl = 'http://huaxia.ad-wizard.cn/mini/userInfo';
         return $this->remoteRequest($targetUrl, $this->jsonBody);
